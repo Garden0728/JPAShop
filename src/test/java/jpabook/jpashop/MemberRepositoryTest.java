aef4519be5ebece2c33777d1dc67d3f9372c37e5
choi jung won <chlwjddnjs56@gmail.com>
2025-10-23 01:35:45 +0900

    add: MemberRepository

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +22,13 @@ class MemberRepositoryTest {
     public void testMember() throws Exception {
         //given
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
         //when
         Long saveId = memberRepository.save(member);
         Member findMember = memberRepository.find(saveId);
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
         Assertions.assertThat(findMember).isEqualTo(member);//TRUE가 나외야하한다. 영속성 컨텍스트에서 식별자가 값이 같으면 같은 엔티티로 인식 즉 영속성 컨텍스트에서 엔티티가 관리가 되고 있는게 이미 있기 때문에 기존에 있던게 나옴
 
     }
