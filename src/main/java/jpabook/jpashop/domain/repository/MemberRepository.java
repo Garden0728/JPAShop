a8fef56287091d988d00710db735e96e077d7df6
choi jung won <chlwjddnjs56@gmail.com>
2025-10-23 23:22:17 +0900

    add: MemberService and apply constructor injection using @RequiredArgsConstructor

--- src/main/java/jpabook/jpashop/domain/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/domain/repository/MemberRepository.java
@@ -3,15 +3,16 @@ package jpabook.jpashop.domain.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @Repository
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
